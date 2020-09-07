408917f231db2cc6ec88a07c5eee990682f77a1e
Rem Blankenburgh <rem.blankenburgh@gmail.com>
2020-09-07 12:55:56 +0200

    Add files via upload

---
+++ Shared/Utils.cs
@@ -0,0 +1,23 @@
+﻿namespace bl2Escalada
+{
+    public enum ApiKind { read, write };
+
+    public class Utils
+    {
+        const string rootUrl = "https://www.remranger.com/escalada-api";
+        public static string GetApiUrl(string apiName, ApiKind kind)
+        {
+            return $"{rootUrl}/{apiName}-{kind}.php";
+        }
+        public static string GetResultPic(int? result)
+        {
+            return result switch
+            {
+                0 => "images/result-fail.png",
+                1 => "images/result-faults.png",
+                2 => "images/result-success.png",
+                _ => null,
+            };
+        }
+    }
+}
